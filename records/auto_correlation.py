4f4ff4d1961bfc1115ac7b74d2d2a59a15ea5e76
wolfram74 <phaugen@gmail.com>
2017-04-14 13:46:03 -0500

    first pass on auto-correlation filter

---
+++ records/auto_correlation.py
@@ -0,0 +1,44 @@
+from matplotlib import pyplot
+import data_loader
+import numpy
+
+def auto_correlation(frequency_data):
+    output = []
+    bin_count = len(frequency_data)
+    for n in range(bin_count):
+        total = 0
+        for offSet in range(bin_count-n):
+            total += frequency_data[n]*frequency_data[offSet]
+        output.append(total)
+    return output
+
+def max_index(data):
+    index= 0
+    value = 0
+    for i in range(len(data)):
+        if data[i] > value:
+            index = i
+            value = data[i]
+    return index
+
+def total_scan(data, keys):
+    picks = []
+    for i in range(len(keys)):
+        frequency_data = data[keys[i]]
+        picks.append(max_index(auto_correlation(frequency_data)))
+    return picks
+
+sample_data = data_loader.load_files()
+times = sorted(sample_data['sample_1.txt'].keys())
+
+single_run = sample_data['sample_1.txt'][times[695]]
+
+sample1_processed = total_scan(sample_data['sample_1.txt'], times)
+# pyplot.plot(single_run)
+# pyplot.show()
+
+pyplot.plot(sample1_processed)
+pyplot.show()
+
+# pyplot.plot(auto_correlation(single_run))
+# pyplot.show()
